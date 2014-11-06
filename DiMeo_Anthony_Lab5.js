6d0168b40e90e8e1f74932b8d554beed0032e97e
AntaineDiMeo <AntaineVEVO@gmail.com>
2014-11-06 18:34:34 -0500

    Added files to the repo.

---
+++ DiMeo_Anthony_Lab5.js
@@ -0,0 +1,47 @@
+alert("JavaScript works!");
+
+/*
+Create four functions. All four functions should accept two number arguments. 
+Each function should perform a different arithmetical operation: division, multiplication, 
+subtraction, and addition. Each function should then return the outcome of the operation.
+
+Next, you'll create prompts that allow a user to enter three items: the two numbers being 
+manipulated and the operation to be performed. For example, if you want to add two numbers together,
+ the first two prompts should accept and parse values as numbers, and the third prompt should accept 
+ something that signifies the operation (such as +. -. /, * or the words add, subtract, multiply, divide). 
+ Include the operation signifier in the prompt so that a user knows what must be entered.
+
+The number returned should be stored in a return value variable and then output to the console in some 
+meaningful way so that a user can see what numbers have been used, what operation was performed, and what the outcome is.
+
+Something to keep in mind: the code should always return meaningful data. Thus, if a user were to try 
+to divide by zero, the code should not throw an error; it should give the user useful feedback.
+*/
+
+
+	var x;
+	var y;
+	var z;
+	
+	
+
+	
+
+		var addition = function(numberOne, numberTwo) {
+			addition = numberOne + numberTwo;
+			return addition;
+			
+};
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
