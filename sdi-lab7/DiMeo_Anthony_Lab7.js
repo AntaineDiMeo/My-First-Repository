3dffb92ef58ababb1e60d92c6b3877cc95b399a0
AntaineDiMeo <AntaineVEVO@gmail.com>
2014-11-11 17:35:17 -0500

    first commit for lab7.

---
+++ sdi-lab7/DiMeo_Anthony_Lab7.js
@@ -0,0 +1,12 @@
+alert("JavaScript works!");
+
+
+var musicList = function(musicArtist, musicGenre, musicSong, musicYear) {
+	
+	this.artist = musicArtist;
+	this.genre = musicGenre;
+	this.song = musicSong;
+	this.year = musicYear;
+};
+
+console.log(musicArtist);
