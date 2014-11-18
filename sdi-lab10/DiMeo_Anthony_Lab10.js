7c2a3445f4ac9ebc6041b34bf83a968f9bd06d1a
AntaineDiMeo <AntaineVEVO@gmail.com>
2014-11-18 17:31:01 -0500

    Problem 1 commit 1.

---
+++ sdi-lab10/DiMeo_Anthony_Lab10.js
@@ -0,0 +1,23 @@
+alert("JavaScript works!");
+
+//Global variables
+
+
+//Problem1 
+//Function
+var returnSum = function() {
+
+for (var i = 0; i < 1000; i++) {
+	
+
+};
+
+//Problem 2
+
+
+
+//Function calls
+returnSum();
+console.log();
+
+
