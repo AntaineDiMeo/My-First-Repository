49e808857f92225a75eb8264c04ea8687624534c
AntaineDiMeo <AntaineVEVO@gmail.com>
2014-11-18 18:57:58 -0500

    second commit.

--- sdi-lab10/DiMeo_Anthony_Lab10.js
+++ sdi-lab10/DiMeo_Anthony_Lab10.js
@@ -1,23 +1,43 @@
 alert("JavaScript works!");
 
 //Global variables
-
-
+var sum = 0;
+var three = 0;
+var five = 0;
+var total;
 //Problem1 
 //Function
 var returnSum = function() {
 
 for (var i = 0; i < 1000; i++) {
-	
-
+	   if (i * 3 == 0) {
+		sum = i;
+} else if (i * 5 == 0) {
+			sum = x;
+		}
+	}
+}
 };
 
 //Problem 2
+var myArray = [ 25,
+				30,
+				19,
+				17,
+				54,
+				1 
+];
 
+var powerball = function() {
+
+
+
+
+};
 
 
 //Function calls
 returnSum();
-console.log();
+console.log(sum);
 
 
