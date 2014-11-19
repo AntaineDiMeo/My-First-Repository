ae7150afcda1fd570b949949eddd45806d50cb31
AntaineDiMeo <AntaineVEVO@gmail.com>
2014-11-18 19:48:56 -0500

    3rd commit

--- sdi-lab10/DiMeo_Anthony_Lab10.js
+++ sdi-lab10/DiMeo_Anthony_Lab10.js
@@ -1,34 +1,52 @@
 alert("JavaScript works!");
 
 //Global variables
+
 var sum = 0;
-var three = 0;
-var five = 0;
-var total;
+
 //Problem1 
 //Function
+
 var returnSum = function() {
 
+
 for (var i = 0; i < 1000; i++) {
-	   if (i * 3 == 0) {
-		sum = i;
-} else if (i * 5 == 0) {
-			sum = x;
-		}
-	}
+  if (i % 3 === 0 || i % 5 === 0) {
+    sum = sum + i;
+  }
 }
 };
 
+
 //Problem 2
+var powerball1;
+var powerball2;
+var powerball3;
+var powerball4;
+var powerball5;
+
 var myArray = [ 25,
 				30,
 				19,
 				17,
 				54,
-				1 
+				1, 
 ];
 
 var powerball = function() {
+	
+	console.log("The numbers are: ");
+	for (var i = 1; i <= 5; i++) {
+		
+var powerball1 = Math.floor(Math.random() * 59 + 1);
+var powerball2 = Math.floor(Math.random() * 59 + 1);
+var powerball3 = Math.floor(Math.random() * 59 + 1);
+var powerball4 = Math.floor(Math.random() * 59 + 1);
+var powerball5 = Math.floor(Math.random() * 59 + 1);
+
+		
+console.log(powerball1 + powerball2 + powerball3 + powerball4 + powerball5);
+	}
 
 
 
@@ -37,7 +55,22 @@ var powerball = function() {
 
 
 //Function calls
-returnSum();
-console.log(sum);
+//Problem 1
+returnSum(sum);
+console.log("There are " + sum + " multiples of the numbers 3 and 5 that are below 1000!");
+
+//Problem2
+powerball();
+console.log("The powerball number is " + myArray[5] + ".");
+
+
+
+
+
+
+
+
+
+
 
 
